82733ce5a586ed58f7eb20be79a57e545ea3e9bd
Schoolmakoto <wolfhacker36@gmail.com>
2024-10-31 13:58:16 -0300

    Updated the code to use environment variables for the databases

--- config/database.js
+++ config/database.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('daipdb', 'postgres', 'password', {
-    host: 'localhost',
-    dialect: 'postgres',
+// Use default values if no environment variable is setted, in this case we will use this to allow unitary tests inside jenkins
+const dbHost = process.env.DB_HOST || 'localhost';
+const dbName = process.env.DB_NAME || 'daipdb'; // Default database name
+const dbUser = process.env.DB_USER || 'postgres'; // Default user
+const dbPassword = process.env.DB_PASSWORD || 'password'; // Default password
+
+// Initialize Sequelize with the defined variables
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,  // Use the dbHost variable
+    dialect: 'postgres', // Adjust dialect as necessary
 });
-///upgrade later to get variables from globalEnvFiles
+
 module.exports = sequelize;
