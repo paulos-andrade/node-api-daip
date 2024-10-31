891e52928a922bd4b711336366fd55679e02a998
Schoolmakoto <wolfhacker36@gmail.com>
2024-10-31 00:26:26 -0300

    put test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -67,4 +67,6 @@ class UserController {
     }
 }
 
+//add patch method
+
 module.exports = UserController;
--- tests/user.test.js
+++ tests/user.test.js
@@ -2,6 +2,7 @@ const request = require("supertest")
 const app = require("../index")
 const reqaddUser = require("./data/user.test.data")
 
+// lets create the base used that will be used in all subsequent tests
 let userId =
     describe("POST /users/createUser", () => {
     test("should create a user", async () => {
@@ -32,5 +33,24 @@ describe("GET users/getById/:id", () => {
             .get(`/users/getById/${userId}`)
             .expect(200)
             .expect('Content-Type', /application\/json/)
+    });
+});
+
+describe("PUT /updateUser/:id", () => {
+    test('should return an updated user', async () => {
+        const updatedTestUserData = {
+            name: "New Name",
+            email: "newemail@test.com"
+        };
+        return request(app)
+            .put(`/users/updateUser/${userId}`)
+            .send(updatedTestUserData)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+            .then(({ body }) => {
+                expect(body.data).toHaveProperty('id', userId);
+                expect(body.data).toHaveProperty('name', updatedTestUserData.name);
+                expect(body.data).toHaveProperty('email', updatedTestUserData.email);
+            });
     });
 });
